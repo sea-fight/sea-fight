15cf3112a6b5d87b6595e361c38fa359d47d4564
BoolmanO <you@example.com>
2024-08-03 15:06:09 +0600

    Improve documentation for server_main.utils.timestamp.__init__.py

--- server-main/server_main/utils/timestamp/__init__.py
+++ server-main/server_main/utils/timestamp/__init__.py
@@ -1,16 +1,33 @@
+"""
+This module provides utility functions for working with date and time in the UTC+0 timezone.
+It includes functions to get the current date and time in UTC, format it as a string, and
+convert date strings to datetime objects in UTC.
+
+Functions:
+    get_utc_datetime: Get the current date and time in UTC+0.
+    get_utc_datetime_str: Get the current date and time in UTC+0 as a formatted string.
+    convert_str_to_datetime: Convert a date string to a datetime object in UTC+0.
+"""
+
 from datetime import datetime, timezone
 
 
 def get_utc_datetime() -> datetime:
     """
-    Returns the current date and time in the UTC+0 timezone.
+    Get the current date and time in the UTC+0 timezone.
+
+    Returns:
+        datetime: The current date and time in UTC+0.
     """
     return datetime.now(timezone.utc)
 
 
 def get_utc_datetime_str() -> str:
     """
-    Returns the current date and time in the UTC+0 timezone.
+    Get the current date and time in the UTC+0 timezone as a formatted string.
+
+    Returns:
+        str: The current date and time in UTC+0 formatted as "%Y-%m-%d %H:%M:%S".
     """
     return datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M:%S")
 
@@ -19,7 +36,14 @@ def convert_str_to_datetime(
     date_str: str, date_format: str = "%Y-%m-%d %H:%M:%S"
 ) -> datetime:
     """
-    Converts a string representation of a date and time to a datetime object with UTC+0 timezone.
+    Convert a string representation of a date and time to a datetime object with UTC+0 timezone.
+
+    Args:
+        date_str (str): The date and time string to convert.
+        date_format (str, optional): The format of the date and time string. Defaults to "%Y-%m-%d %H:%M:%S".
+
+    Returns:
+        datetime: A datetime object representing the specified date and time in UTC+0.
     """
     local_dt = datetime.strptime(date_str, date_format)
     utc_dt = local_dt.replace(tzinfo=timezone.utc)
